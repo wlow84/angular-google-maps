02a2ec04fb04bfb84f0b1a1b81fdcac81202c877
Wai Yong Low <waiyong.low.wy@gmail.com>
2012-11-22 13:51:44 -0500

    adding mapping functionality to the clean seed file

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,78 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives'])
+
+//routes
+myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2', controller: MyCtrl2});
     $routeProvider.otherwise({redirectTo: '/view1'});
     $locationProvider.html5Mode(true);
-  }]);
+}]);
+
+//factory method that returns a google map
+//instance that we use for drawing
+myApp.factory('maps', function(){
+    //local vars
+    var map;
+    var geocoder;
+    var drawingManager;
+
+    //function to initialize the map
+    function initialize(){
+        geocoder =  new google.maps.Geocoder();
+        var mapOptions = {
+            zoom: 17,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+
+        drawingManager = new google.maps.drawing.DrawingManager({
+            drawingMode: null,
+            drawingControl: true,
+            drawingControlOptions: {
+                position: google.maps.ControlPosition.TOP_CENTER,
+                drawingModes: [
+                    google.maps.drawing.OverlayType.POLYGON
+                ]
+            },
+            polygonOptions: {
+                fillColor: '#ffff00',
+                fillOpacity: 0.5,
+                strokeWeight: 1,
+                clickable: true,
+                zIndex: 1,
+                editable: true
+            }
+        });
+        drawingManager.setMap(map);
+    }
+
+    initialize();
+
+    return {
+        //sets the map location to address specified
+        setMapLocation:function(address){
+            geocoder.geocode( { 'address': address}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    map.setCenter(results[0].geometry.location);
+                } else {
+                    alert('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+        },
+        //add handler to polygoncomplete
+        setPolygonCompleteHandler:function(handler){
+            google.maps.event.addListener(drawingManager, 'polygoncomplete', handler);
+        },
+        //show the polygon
+        showPolygon:function(polygon){
+            polygon.setMap(map);
+        },
+        //hide the polygon
+        hidePolygon:function(polygon){
+            polygon.setMap(null);
+        }
+    };
+});
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,19 +3,57 @@
 /* Controllers */
 
 function AppCtrl($scope, $http) {
-  $http({method: 'GET', url: '/api/name'}).
-  success(function(data, status, headers, config) {
-    $scope.name = data.name;
-  }).
-  error(function(data, status, headers, config) {
-    $scope.name = 'Error!'
-  });
+    $http({method: 'GET', url: '/api/name'}).
+        success(function(data, status, headers, config) {
+            $scope.name = data.name;
+        }).
+        error(function(data, status, headers, config) {
+            $scope.name = 'Error!';
+        });
 }
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+function MyCtrl1($scope, maps) {
+    //model props
+    $scope.showAddressBar = false;
+    $scope.address = 'Mall of america';
+    $scope.showStoreBar = false;
+    $scope.stores = [];
+
+    //toggles the display of an overlay
+    $scope.toggleOverlay = function(index){
+        var store = $scope.stores[index];
+        if(store.showOverlay){
+            maps.showPolygon(store.polygon);
+        }
+        else{
+            maps.hidePolygon(store.polygon);
+        }
+        return;
+    }
+
+    //delete a polygon from scope
+    $scope.deleteStore = function(index){
+        var store = $scope.stores[index];
+        maps.hidePolygon(store.polygon);
+        $scope.stores.splice(index,1);
+    };
+
+    //handler to capture the polygon completed event
+    $scope.polygonCompleted = function(polygon){
+        $scope.$apply(function(){
+            var path = polygon.getPath().getArray();
+            $scope.stores.push({name:"", vertices:path, polygon:polygon, showOverlay:true});
+            $scope.showStoreBar = true;
+
+        });
+    };
+
+    maps.setMapLocation($scope.address);
+    maps.setPolygonCompleteHandler($scope.polygonCompleted);
+}
+//MyCtrl1.$inject = [];
 
 
 function MyCtrl2() {
 }
-MyCtrl2.$inject = [];
+//MyCtrl2.$inject = [];
